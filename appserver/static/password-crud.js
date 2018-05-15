4cb6f8c57366e480ea7fecd8d61dd02061da1ada
Scott Haskell <shaskell@splunk.com>
2018-05-15 11:45:26 -0700

    accidentally removed renderCreateUserForm thinking it was a superfluous renderUpdateUserForm

--- appserver/static/password-crud.js
+++ appserver/static/password-crud.js
@@ -644,6 +644,159 @@ function ($,
         }
     }
 
+
+    function renderCreateUserForm(cUsername = false, cRealm = false) {
+        var createUser = function createUser() {
+            event.preventDefault();
+        
+            var aclData = {};
+
+            _.each(arguments[2], function(component, i) {
+                var aclKey = component.config.aclKey;
+                aclData[aclKey] = _.isArray(component.getVals()) ? component.getVals().join():component.getVals();
+            });
+
+            var username = $('input[id=createUsername]').val();
+            var password = $('input[id=createPassword]').val();
+            var confirmPassword = $('input[id=createConfirmPassword]').val();
+            var realm = $('input[id=createRealm]').val();
+            
+            if(username == "") {
+                return renderModal("missing-username",
+                                    "Missing Username",
+                                    "<div class=\"alert alert-error\"><i class=\"icon-alert\"></i><p>Please enter a username</b></div>",
+                                    "Close",
+                                    renderCreateUserForm);
+            }
+
+            if(password == "") {
+                return renderModal("missing-password",
+                                    "Missing Password",
+                                    "<p>Please enter a password</b>",
+                                    "Close",
+                                    renderCreateUserForm);
+            }
+
+            // Create object to POST for user creation
+            var createData = {"name": username,
+                              "password": password,
+                              "realm": realm};
+
+            if(password != confirmPassword) {
+                return renderModal("password-mismatch",
+                                    "Password Mismatch",
+                                    "<p>Passwords do not match</b>",
+                                    "Close",
+                                    renderCreateUserForm,
+                                    [username, realm]);
+            } else {
+                var createUrl = "/en-US/splunkd/__raw/servicesNS/" + aclData.owner + "/" + aclData.app + "/storage/passwords";
+                var aclUrl = "/en-US/splunkd/__raw/servicesNS/" + aclData.owner + "/" + aclData.app + "/configs/conf-passwords/credential%3A" + realm + "%3A" + username + "%3A/acl";
+
+                 
+
+                // Success message for final modal display
+                var message = [];
+
+                console.log(aclData);
+
+                $.ajax({
+                    type: "POST",
+                    url: createUrl,
+                    data: createData,
+                    success: function() {
+                        message.push("<div><i class=\"icon-check-circle\"></i> Successfully created user <b>" + realm + ":" + username + "</b></div>");
+                    },
+                    error: function(e) {
+                        message.push("<div class=\"alert alert-error\"><i class=\"icon-alert\"></i>Failed to create user " + username + ":" + realm + "</p><br><p>" + e.responseText + "</div>");
+                    }
+                })
+                .then(function() {
+                    // App not a valid key for updating Splunk ACL's, remove it before posting
+                    delete aclData.app;
+
+                    return $.ajax({
+                        type: "POST",
+                        url: aclUrl,
+                        data: aclData,
+                        success: function() {
+                            message.push("<div><i class=\"icon-check-circle\"></i> Successfully applied ACL's</div>")
+                        },
+                        error: function(e) {
+                            message.push("<div class=\"alert alert-error\"><i class=\"icon-alert\"></i>Failed to apply ACL</p><br><p>" + e.responseText + "</div>");
+                        }
+                    })                
+                })
+                .done(function () {
+                    renderModal("user-created",
+                                "User Created",
+                                message.join('\n'),
+                                "Close",
+                                refreshWindow)
+                });
+            }
+        }
+
+        var inputs = [new splunkJSInput({"id": "app-scope-dropdown",
+                       "searchString": "| rest /servicesNS/-/-/apps/local | rename title as value | table label, value",
+                       "el": "app-scope-dropdown",
+                       "type": "dropdown",
+                       "default": utils.getCurrentApp(),
+                       "aclKey": "app",
+                       "parentEl": "app-scope"}),
+                       new splunkJSInput({"id": "read-user-multi",
+                        "searchString": "| rest /servicesNS/-/-/authorization/roles | eval label=title | rename title as value | fields label, value | append [| rest /servicesNS/-/-/authentication/users | eval label=title | rename title as value | fields label, value] | dedup label",
+                        "el": "read-user-multi",
+                        "type": "multi-dropdown",
+                        "default": "*",
+                        "aclKey": "perms.read",
+                        "parentEl": "read-users"}),
+                       new splunkJSInput({"id": "write-user-multi",
+                        "searchString": "| rest /servicesNS/-/-/authorization/roles | eval label=title | rename title as value | fields label, value | append [| rest /servicesNS/-/-/authentication/users | eval label=title | rename title as value | fields label, value] | dedup label",
+                        "el": "write-user-multi",
+                        "type": "multi-dropdown",
+                        "parentEl": "write-users",
+                        "aclKey": "perms.write",
+                        "default": ["admin","power"]}),
+                       new splunkJSInput({"id": "sharing-dropdown",
+                        "choices": [{"label":"global", "value": "global"},
+                                    {"label":"app", "value": "app"},
+                                    {"label":"user", "value": "user"}],
+                        "el": "sharing-dropdown",
+                        "type": "dropdown",
+                        "parentEl": "sharing",
+                        "aclKey": "sharing",
+                        "default": "app"}),
+                       new splunkJSInput({"id": "owner-dropdown",
+                        "searchString": "| rest /servicesNS/-/-/authentication/users | eval label=title | rename title as value | table label, value",
+                        "el": "owner-dropdown",
+                        "type": "dropdown",
+                        "aclKey": "owner",
+                        "default": Splunk.util.getConfigValue("USERNAME"),
+                        "parentEl": "owner"})];
+
+        // Remove component if it exists
+        _.each(inputs, function(input, i) {
+            input.remove();
+        });
+
+        // Fire searches and render splunkJS form components to modal
+        $.when(execMultiSearch(inputs)).done(function(components) {
+            _.each(components, function(component, i) {
+                component.waitForElAndRender();
+            });
+
+            clearOnClickAndRegister('#create-submit', createUser, [cUsername, cRealm, inputs]);
+        });
+    
+        setTimeout(function () {
+            if(cUsername != "" || cRealm != "") {
+                $('input[id=createUsername]').val(cUsername);
+                $('input[id=createRealm]').val(cRealm);
+            }
+        }, 300);
+    }
+
     function renderUpdateUserInTable(row) {
         var updateUser = function updateUser () {
             event.preventDefault();
