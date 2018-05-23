fbf16e04adb365f4cda68f80200a626695bce2f8
Scott Haskell <shaskell@splunk.com>
2018-05-23 08:49:35 -0700

    Removed hard coding static backdrop and keyboard disabled options in the Modal class. Those should be passed in when the modal is created

--- appserver/static/Modal.js
+++ appserver/static/Modal.js
@@ -46,10 +46,7 @@ define(['underscore'], function (_) {
 
             _classCallCheck(this, Modal);
 
-            var modalOptions = _.extend({ show: false,
-                                          backdrop: 'static',
-                                          keyboard: false
-                                        }, options);
+            var modalOptions = _.extend({ show: false}, options);
 
             // if "id" is the element that triggers the modal display, extract the actual id from it; otherwise use it as-is
             var modalId = id != null && (typeof id === 'undefined' ? 'undefined' : _typeof(id)) === 'object' && id.jquery != null ? id.attr('data-target').slice(1) : id;
